c674ea4c82cccf1e9766b788a680d3e9fb06eaa5
Omar Alvarez <omajul2@hotmail.com>
2016-06-01 18:29:30 +0200

    Finish step 12 - Add complete button for each todo

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -2,8 +2,8 @@ describe('Todos tracker', function() {
   it('has severals todos', function(){
   	browser.get('/');
   	var todos = $$('#todos p');
-  	expect(todos.first().getText()).toEqual('ToDo1: completed');
-  	expect(todos.last().getText()).toEqual('ToDo2: not completed');
+  	expect(todos.first().getText()).toMatch('ToDo1: completed');
+  	expect(todos.last().getText()).toMatch('ToDo2: not completed');
   });
 
   it('adds a new todo', function(){
@@ -11,14 +11,22 @@ describe('Todos tracker', function() {
   	var todos = $$('#todos p');
   	$("#todo_name").sendKeys("Clean the garage");
   	$("#add_todo").click();
-  	expect(todos.last().getText()).toEqual('Clean the garage: not completed');
+  	expect(todos.last().getText()).toMatch('Clean the garage: not completed');
   });
 
   it('removes the last todo', function(){
   	browser.get('/');
   	var todos = $$('#todos p');
   	$("#remove_todo").click();
-  	expect(todos.last().getText()).toEqual('ToDo1: completed');
+  	expect(todos.last().getText()).toMatch('ToDo1: completed');
   	expect(todos.count()).toEqual(1);
   });
+
+  it('can mark a todo as complete', function(){
+  	browser.get('/');
+  	var todo = $$('#todos p').last();
+  	todo.element(by.css('.complete')).click();
+  	expect(todo.getText()).toMatch('ToDo2: completed');
+  });
+
 });
