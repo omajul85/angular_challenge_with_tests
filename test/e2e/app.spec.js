6d47e23f2ec2adc5c398663ff5ad8692cc39bf49
Omar Alvarez <omajul2@hotmail.com>
2016-06-01 08:57:31 +0200

    First couple of tests passing

--- test/e2e/app.spec.js
+++ test/e2e/app.spec.js
@@ -1,6 +1,11 @@
 describe("app", function() {
   it("should get home page title", function() {
     browser.get('/');
-    expect(browser.getTitle()).toEqual("Angular Challenge with tests");
+    expect(browser.getTitle()).toEqual("Todos App");
+  });
+
+  it("should say 'Hello world' on the page", function() {
+    browser.get('/');
+    expect($$("p").first().getText()).toEqual("Hello world");
   });
 });
